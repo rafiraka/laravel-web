755bd34318bff0e73a2fdfa4e9d8b78e7260a826
rafiraka <rafi24ti@mahasiswa.pcr.ac.id>
2025-12-01 15:10:34 +0700

    wqwqwqw

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -22,52 +22,76 @@
             </div>
             <!-- Navbar links -->
             <ul class="navbar-nav align-items-center">
-                <li class="nav-item dropdown">
-                    <a class="nav-link nav-item dropdown-toggle ms-lg-3 pt-1 px-0" href="#" role="button"
+                <li class="nav-item dropdown"></li>
+
+                <li class="nav-item dropdown ms-lg-3">
+                    <a class="nav-link dropdown-toggle pt-1 px-0" href="#" role="button"
                         data-bs-toggle="dropdown" aria-expanded="false">
-                        <div class="class media d-flex align-items-center">
+                        <div class="media d-flex align-items-center">
                             <img class="avatar rounded-circle" alt="Image placeholder"
                                 src="{{ asset('assets-admin/img/team/profile-picture-3.jpg') }}">
-                            <div class="class media-body ms-2 text-dark align-items-center d-none d-lg-block">
-                                <span class="mb-0 font-small fw-bold text-gray-900">{{ Auth::user()->name }}</span>
+                            <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
+                                <span class="mb-0 font-small fw-bold text-gray-900">
+                                    @auth
+                                        <span class="ms-0 font-small fw-bold text-gray-900"> {{ Auth::user()->name }}
+                                        </span>
+                                    @endauth
+                                </span>
                             </div>
                         </div>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-end mt-2 py-1">
+
+                    <div class="dropdown-menu dashboard-dropdown dropdown-menu-end mt-2 py-1">
+
                         <a class="dropdown-item d-flex align-items-center" href="#">
                             <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M10 8a1 1 0 0 0-1 1v.793l-.707.707a1 1 0 0 0 1.414 1.414l.707-.707V12a1 1 0 0 0 2 0v-.207l.707.707a1 1 0 0 0 1.414-1.414l-.707-.707V9a1 1 0 0 0-1-1zm-2 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v1h-4v-1zM2 6a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm4 0h-2v2h2V6zM16 12a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4zm-2 0h-2v2h2v-2zM4 14a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zM16 14a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-4z"
-                                    clip-rule="evenodd"></path>
+                                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0
+                             2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986
+                             5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5
+                             5 0 0010 11z" clip-rule="evenodd"></path>
                             </svg>
                             My Profile
                         </a>
+
                         <a class="dropdown-item d-flex align-items-center" href="#">
                             <svg class="dropdown-icon text-gray-400 me-2" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M11.49 3.17c-.38-.21-.78-.38-1.56-.63-.26-.08-.52-.16-.94-.17-.42 0-.68.08-.94.17-.78.25-1.18.42-1.56.63-.38.21-1.4.92-2.18 1.42-.78.5-1.56 1.15-2.28 1.83-1.44 1.34-2.16 2.76-2.16 4.38 0 1.62.72 3.04 2.16 4.38.72.68 1.5 1.33 2.28 1.83.78.5 1.8 1.21 2.18 1.42.38.21.78.38 1.56.63.26.08.52.16.94.17.42 0 .68-.08.94-.17.78-.25 1.18-.42 1.56-.63.38-.21 1.4-.92 2.18-1.42.78-.5 1.56-1.15 2.28-1.83 1.44-1.34 2.16-2.76 2.16-4.38 0-1.62-.72-3.04-2.16-4.38-.72-.68-1.5-1.33-2.28-1.83-.78-.5-1.8-1.21-2.18-1.42zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"
-                                    clip-rule="evenodd"></path>
+                                <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98
+                             0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-
+                             2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-
+                             1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836
+                             1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379
+                             1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836
+                             2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379
+                             1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-
+                             2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0
+                             100-6 3 3 0 000 6z" clip-rule="evenodd"></path>
                             </svg>
                             Settings
                         </a>
+
                         <div role="separator" class="dropdown-divider my-1"></div>
+
                         <a class="dropdown-item d-flex align-items-center" href="#"
-                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <svg class="dropdown-icon text-danger me-2" fill="none" stroke="currentColor"
                                 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6-4v4m3 0v4m3 0v4m3 0v4"></path>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6
+                             4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3
+                             3 0 013-3h4a3 3 0 013 3v1"></path>
                             </svg>
                             Logout
                         </a>
+
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                             @csrf
                         </form>
+
                     </div>
                 </li>
             </ul>
+
         </div>
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,66 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\PelangganController;
-use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\UserController;
 
-//halaman guest
-Route::middleware('guest')->group(function () {
-    // Halaman Form Login
-    Route::get('/auth', [AuthController::class, 'index'])->name('login');
+Route::get('/', function () {
+    return view ('welcome');
 
-    // Proses Submit Login
-    Route::post('/auth/login', [AuthController::class, 'login'])->name('login.process');
-
-    // Halaman Depan
-    Route::get('/', function () {
-        return view('welcome');
-    });
 });
 
-//halaman wajib login
+Route::get('/pcr', function () {
+    return 'selamat datang di website kampus PCR!';
+});
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'nim saya: '.$param1;
+});
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+//matakuliah
+Route::get('/matakuliah', [MatakuliahController::class, 'index'])->name('matakuliah');
+Route::get('/matakuliah/show/{kode?}', [MatakuliahController::class, 'show']);
+
+//home
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+//question
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
+
+//auth
+Route::get('/auth', [AuthController::class, 'index'])->name('auth.index');
+Route::post('/auth/login', [AuthController::class, 'login'])->name('auth.login');
+
+//pegawai
+Route::get('/pegawai', [PegawaiController::class, 'index'])->name('pegawai.index');
+
+//dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+//pelanggan
+Route::resource('pelanggan', PelangganController::class);
+
+//user
+Route::resource('user', UserController::class);
+
 Route::middleware('auth')->group(function () {
     // Logout (Bisa diakses semua user yang login)
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
@@ -32,11 +70,13 @@ Route::middleware('auth')->group(function () {
 
     // Fitur User Biasa (Contoh: Kirim Pertanyaan)
     Route::post('question/store', [QuestionController::class, 'store'])->name('question.store');
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('home', [HomeController::class, 'index']); // (Note: Ini bisa jadi rute alternatif untuk dashboard)
 
     // Khusus admin
-    Route::middleware('role:admin')->prefix('admin')->group(function () {
+    Route::middleware(['role:admin'])->prefix('admin')->group(function () {
         Route::resource('user', UserController::class);
         Route::resource('pelanggan', PelangganController::class);
     });
 });
+
+
