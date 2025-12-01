1e96ff3259fcf28bf1570b40627b78aa0c1ef215
rafiraka <rafi24ti@mahasiswa.pcr.ac.id>
2025-12-01 14:20:18 +0700

    1/12/25/1

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        // Reset cache permission (Wajib agar tidak error)
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // role dengan huruf kecil
+        $roleAdmin = Role::firstOrCreate(['name' => 'admin']);
+        $roleUser = Role::firstOrCreate(['name' => 'user']);
+
+        // Buat Akun ADMIN
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@gmail.com'], // Cek email biar gak duplikat
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('password'), // Passwordnya: password
+            ]
+        );
+        $admin->assignRole($roleAdmin); // <--- Kasih jabatan Admin
+
+        // Buat Akun USER BIASA
+        $user = User::firstOrCreate(
+            ['email' => 'user@gmail.com'],
+            [
+                'name' => 'User Biasa',
+                'password' => Hash::make('password'),
+            ]
+        );
+        $user->assignRole($roleUser); // // <--- Kasih jabatan User
+    }
+}
